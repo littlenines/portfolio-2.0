969b54076b349f5ddd308aa4c4bbd7a4acadfcc5
Nemanja <nemanja584@hotmail.com>
2022-10-24 23:44:08 +0200

    add intro section

--- pages/index.vue
+++ pages/index.vue
@@ -8,13 +8,46 @@
 
         <div class="intro-wrapper">
           <div class="nav-wrapper">
-            Nav
+            <div class="dots-wrapper">
+              <div id="dot-1" class="browser-dot" />
+              <div id="dot-2" class="browser-dot" />
+              <div id="dot-3" class="browser-dot" />
+            </div>
+
+            <ul id="navigation">
+              <li><a href="">Contact</a></li>
+            </ul>
           </div>
           <div class="left-column">
-            left
+            <img id="profile_pic" src="~/assets/img/hero-picture.png" alt="hero img">
+            <h5 style="text-align:center; line-height: 0;">
+              Personalize Theme
+            </h5>
+
+            <div id="theme-options-wrapper">
+              <div id="light-mode" class="theme-dot" />
+              <div id="blue-mode" class="theme-dot" />
+              <div id="green-mode" class="theme-dot" />
+              <div id="purple-mode" class="theme-dot" />
+            </div>
+
+            <p id="settings-note">
+              *Theme settings will be saved for your next visit
+            </p>
           </div>
           <div class="right-column">
-            right
+            <div id="preview-shadow">
+              <div id="preview">
+                <div id="corner-tl" class="corner" />
+                <div id="corner-tr" class="corner" />
+
+                <h3>What I Do</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque illo fugiat aspernatur fuga doloremque. Corporis!</p>
+
+                <div id="corner-br" class="corner" />
+                <div id="corner-bl" class="corner" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
